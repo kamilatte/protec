891c1341b09a7b4686b6ae3b1de0d9f097e4cb75
alwayshyper <118631823+alwayshyper@users.noreply.github.com>
2023-07-21 18:06:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,128 @@
+import os
+import sys
 import subprocess
+import requests
 import PySimpleGUI as sg
+import qrcode
 import pyotp
-import time
 
 KEYCHAIN_SERVICE_NAME = "com.example.app_password"
-TWO_FACTOR_SECRET = "your_secret_key"  # Replace this with your own secret key
+OTP_SECRET_KEY_FILENAME = "otp_secret_key.txt"
 
 def set_password(password):
-    cmd = f'security add-generic-password -s {KEYCHAIN_SERVICE_NAME} -a app_user -w "{password}"'
-    subprocess.run(cmd, shell=True)
+    if sys.platform == "darwin":
+        cmd = f'security add-generic-password -s {KEYCHAIN_SERVICE_NAME} -a app_password -w "{password}"'
+        subprocess.run(cmd, shell=True)
+    else:
+        # Add code for storing password on other platforms here
+        pass
 
 def get_expected_password():
-    cmd = f'security find-generic-password -s {KEYCHAIN_SERVICE_NAME} -a app_user -w'
-    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-    return result.stdout.strip()
+    if sys.platform == "darwin":
+        cmd = f'security find-generic-password -s {KEYCHAIN_SERVICE_NAME} -a app_password -w'
+        result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+        return result.stdout.strip()
+    else:
+        # Add code for getting password on other platforms here
+        pass
 
-def generate_totp_token():
-    totp = pyotp.TOTP(TWO_FACTOR_SECRET)
-    return totp.now()
-
-def login():
-    # Get the expected password from the keychain
-    expected_password = get_expected_password()
-
+def check_additional_password(expected_password):
     layout = [
-        [sg.Text("Username:"), sg.Input(key="-USERNAME-")],
-        [sg.Text("Password:"), sg.Input(key="-PASSWORD-", password_char="*")],
-        [sg.Text("Two-Factor Code:"), sg.Input(key="-TOTP-")],
-        [sg.Button("Login")]
+        [sg.Text("Please enter the additional password:")],
+        [sg.Input(key="-PASSWORD-", password_char="*")],
+        [sg.Button("OK")]
     ]
 
-    window = sg.Window("Login", layout)
+    window = sg.Window("Password Check", layout)
 
     while True:
         event, values = window.read()
-
         if event == sg.WIN_CLOSED:
             break
-
-        if event == "Login":
-            username = values["-USERNAME-"]
-            password = values["-PASSWORD-"]
-            totp_code = values["-TOTP-"]
-
-            if password == expected_password:
-                # Verify TOTP token
-                totp = pyotp.TOTP(TWO_FACTOR_SECRET)
-                if totp.verify(totp_code):
-                    sg.popup("Login successful!")
-                    # Add your code to continue with the login process here
-                else:
-                    sg.popup("Incorrect TOTP code. Login failed.")
+        elif event == "OK":
+            additional_password = values["-PASSWORD-"]
+            if additional_password == expected_password:
+                sg.popup("Login successful!")
+                # Add your code to continue with the login process here
             else:
                 sg.popup("Incorrect password. Login failed.")
+            break
 
     window.close()
 
+def self_update():
+    # ... (same as before)
+
+def generate_qr_code(secret_key):
+    totp = pyotp.TOTP(secret_key)
+    qr_data = totp.provisioning_uri(name="App User", issuer_name="Example")
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(qr_data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrcode.png")
+
+def store_2fa_key_in_keychain(secret_key):
+    if sys.platform == "darwin":
+        cmd = f'security add-generic-password -s {KEYCHAIN_SERVICE_NAME} -a 2fa_key -w "{secret_key}"'
+        subprocess.run(cmd, shell=True)
+    else:
+        # Add code for storing 2FA key in keychain on other platforms here
+        pass
+
+def setup_2fa():
+    secret_key = pyotp.random_base32()
+    generate_qr_code(secret_key)
+    sg.popup("Scan or enter the 2FA key:")
+    store_2fa_key_in_keychain(secret_key)
+    os.remove("qrcode.png")
+
+def get_2fa_key():
+    if sys.platform == "darwin":
+        cmd = f'security find-generic-password -s {KEYCHAIN_SERVICE_NAME} -a 2fa_key -w'
+        result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+        return result.stdout.strip() if result.returncode == 0 else None
+    else:
+        # Add code for getting 2FA key from keychain on other platforms here
+        pass
+
+def prompt_for_2fa():
+    layout = [
+        [sg.Text("Please enter your 2FA password:")],
+        [sg.Input(key="-PASSWORD-", password_char="*")],
+        [sg.Button("OK")]
+    ]
+
+    window = sg.Window("2FA Password", layout)
+
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
+            sg.popup("2FA setup canceled. The app will not run without 2FA.")
+            sys.exit(1)
+        elif event == "OK":
+            return values["-PASSWORD-"]
+
+    window.close()
+
+def login():
+    expected_password = get_expected_password()
+
+    if expected_password is None:
+        sg.popup("2FA is not set up. Please set up 2FA to continue.")
+        setup_2fa()
+
+    additional_password = prompt_for_2fa()
+
+    if additional_password == expected_password:
+        sg.popup("Login successful!")
+        # Add your code to continue with the login process here
+    else:
+        sg.popup("Incorrect password. Login failed.")
+        sys.exit(1)
+
 def main():
-    # ... (rest of the code)
+    # ... (same as before)
 
 if __name__ == "__main__":
     main()
