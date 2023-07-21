e139a9eccc68dd9354c5b70b3175bb921c21e65c
alwayshyper <118631823+alwayshyper@users.noreply.github.com>
2023-07-21 16:36:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,78 +1,63 @@
-import os
-import sys
 import subprocess
-import requests
 import PySimpleGUI as sg
+import pyotp
+import time
 
 KEYCHAIN_SERVICE_NAME = "com.example.app_password"
+TWO_FACTOR_SECRET = "your_secret_key"  # Replace this with your own secret key
 
 def set_password(password):
-    if sys.platform == "darwin":
-        cmd = f'security add-generic-password -s {KEYCHAIN_SERVICE_NAME} -a app_user -w "{password}"'
-        subprocess.run(cmd, shell=True)
-    else:
-        # Add code for storing password on other platforms here
-        pass
+    cmd = f'security add-generic-password -s {KEYCHAIN_SERVICE_NAME} -a app_user -w "{password}"'
+    subprocess.run(cmd, shell=True)
 
 def get_expected_password():
-    if sys.platform == "darwin":
-        cmd = f'security find-generic-password -s {KEYCHAIN_SERVICE_NAME} -a app_user -w'
-        result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-        return result.stdout.strip()
-    else:
-        # Add code for getting password on other platforms here
-        pass
+    cmd = f'security find-generic-password -s {KEYCHAIN_SERVICE_NAME} -a app_user -w'
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    return result.stdout.strip()
 
-def check_additional_password(expected_password):
+def generate_totp_token():
+    totp = pyotp.TOTP(TWO_FACTOR_SECRET)
+    return totp.now()
+
+def login():
+    # Get the expected password from the keychain
+    expected_password = get_expected_password()
+
     layout = [
-        [sg.Text("Please enter the additional password:")],
-        [sg.Input(key="-PASSWORD-", password_char="*")],
-        [sg.Button("OK")]
+        [sg.Text("Username:"), sg.Input(key="-USERNAME-")],
+        [sg.Text("Password:"), sg.Input(key="-PASSWORD-", password_char="*")],
+        [sg.Text("Two-Factor Code:"), sg.Input(key="-TOTP-")],
+        [sg.Button("Login")]
     ]
 
-    window = sg.Window("Password Check", layout)
+    window = sg.Window("Login", layout)
 
     while True:
         event, values = window.read()
+
         if event == sg.WIN_CLOSED:
             break
-        elif event == "OK":
-            additional_password = values["-PASSWORD-"]
-            if additional_password == expected_password:
-                sg.popup("Login successful!")
-                login()  # Call the login function on successful login
+
+        if event == "Login":
+            username = values["-USERNAME-"]
+            password = values["-PASSWORD-"]
+            totp_code = values["-TOTP-"]
+
+            if password == expected_password:
+                # Verify TOTP token
+                totp = pyotp.TOTP(TWO_FACTOR_SECRET)
+                if totp.verify(totp_code):
+                    sg.popup("Login successful!")
+                    # Add your code to continue with the login process here
+                else:
+                    sg.popup("Incorrect TOTP code. Login failed.")
             else:
                 sg.popup("Incorrect password. Login failed.")
-            break
 
     window.close()
 
-def self_update():
-    # ... (same as before)
-
-def login():
-    # Implement your login logic here
-    # For example, you can prompt the user for credentials and check them against some stored data
-    # Replace the following line with your login implementation:
-    username = sg.popup_get_text("Enter your username:")
-    password = sg.popup_get_text("Enter your password:", password_char="*")
-    if username == "your_username" and password == "your_password":
-        sg.popup("Login successful!")
-    else:
-        sg.popup("Incorrect username or password. Login failed.")
-
 def main():
-    if sys.platform == "darwin":
-        if not get_expected_password():
-            password = sg.popup_get_text("Set the expected password:", password_char="*")
-            set_password(password)
-
-        expected_password = get_expected_password()
-        check_additional_password(expected_password)
-
-    else:
-        sg.popup("This script only supports macOS.")
-        sys.exit(1)
+    # ... (rest of the code)
 
 if __name__ == "__main__":
     main()
