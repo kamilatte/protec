400c113ce4ca83b7273f69e0fc17c788d5160e5a
kamilatte <118631823+kamilatte@users.noreply.github.com>
2023-11-02 07:56:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
-#!/bin/bash
-
-# Function to create the .myscript directory if it doesn't exist
+#im not doing that lol IYKYK
 function create_myscript_directory() {
     echo "Creating .myscript directory..."
     mkdir -p "$HOME/.myscript"
 }
 
-# Function to install Homebrew if it's not already installed
 function install_homebrew() {
     echo "Checking if Homebrew is installed..."
     if ! command -v brew &>/dev/null; then
@@ -22,13 +19,11 @@ function install_homebrew() {
     fi
 }
 
-# Function to install Python with Tkinter support using Homebrew
 function install_python_tkinter() {
     echo "Installing Python with Tkinter support..."
     brew install python-tk || display_error "Failed to install Python with Tkinter support"
 }
 
-# Function to install get-pip.py
 function install_get_pip() {
     echo "Installing get-pip.py..."
     curl -fsSL https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py || display_error "Failed to download get-pip.py"
@@ -36,24 +31,18 @@ function install_get_pip() {
     export PATH="/Library/Frameworks/Python.framework/Versions/3.11/bin:$PATH"
 }
 
-# Function to install PySimpleGUI and required packages
 function install_python_packages() {
     echo "Installing PySimpleGUI and required packages..."
 
-    # Upgrade pip
     /usr/local/bin/python3 -m pip install --upgrade pip || display_error "Failed to upgrade pip"
-
-    # Install other required packages
     /usr/local/bin/python3 -m pip install PySimpleGUI qrcode requests pyotp pillow keyring || display_error "Failed to install required Python packages"
 }
 
-# Function to download the Python script
 function download_script() {
     echo "Downloading the Python script..."
     curl -o "$HOME/.myscript/new.py" https://raw.githubusercontent.com/alwayshyper/protec/main/main.py || display_error "Failed to download the Python script"
 }
 
-# Function to create the shell script
 function create_shell_script() {
     echo "Creating the shell script..."
     cat << EOF > "$HOME/.myscript/run_new_script.sh"
@@ -66,11 +55,9 @@ cd "$HOME/.myscript/"
 /usr/local/bin/python3 new.py
 EOF
 
-    # Make the shell script executable
     chmod +x "$HOME/.myscript/run_new_script.sh"
 }
 
-# Function to install the launch agent
 function install_launch_agent() {
     echo "Creating a launch agent..."
     cat << EOF > "$HOME/Library/LaunchAgents/com.$USER.run_new_script.plist"
@@ -92,26 +79,22 @@ function install_launch_agent() {
 EOF
 }
 
-# Function to load the launch agent
 function load_launch_agent() {
     echo "Loading the launch agent..."
     launchctl load "$HOME/Library/LaunchAgents/com.$USER.run_new_script.plist" || display_error "Failed to load the launch agent"
 }
 
-# Function to prompt the user for 2FA password
 function prompt_for_2fa() {
     echo "Please enter your 2FA password:"
     read -r password
     echo "$password"
 }
 
-# Function to display an error message and exit with a non-zero status
 function display_error() {
     echo "Error: $1"
     exit 1
 }
 
-# Main execution
 function main() {
     create_myscript_directory
     install_homebrew
@@ -123,7 +106,6 @@ function main() {
     install_launch_agent
     load_launch_agent
     
-    # Prompt the user for 2FA password after installation
     password=$(prompt_for_2fa)
     echo "Setting up 2FA..."
     python3 "$HOME/.myscript/new.py" setup_2fa "$password"
@@ -131,5 +113,5 @@ function main() {
     echo "Setup completed successfully!"
 }
 
-# Execute main function
+# Я устал:/
 main
